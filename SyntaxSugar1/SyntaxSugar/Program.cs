58e9a315bbcac5e109a38c82cedb519615f8e079
bluebeacon8402 <crsorge@icloud.com>
2020-10-27 14:18:00 -0400

    new exercise

---
+++ SyntaxSugar1/SyntaxSugar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + “greater than or equal to nine”;
+            }*/
+
+            var answer = 4; string response;
+            if (answer < 9) { response = answer + "is less than nine"; }
+            else { response = answer + "greater than or equal to nine"; }
+
+            Console.WriteLine($"{response}");
+
+
+
+        }
+    }
+}
